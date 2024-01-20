e4cde33ab1d3d269737fd1d0993ba2918548d2ef
DiegoA <diegoalex970006@gmail.com>
2024-01-20 09:10:03 -0500

    Add new feature: age calc

--- welcome.py
+++ welcome.py
@@ -1,7 +1,10 @@
 nombre = "Diego"
 city = "Pasto"
+birthday = 1989
+now = 2024
 print(nombre, city)
 
 name = "Diego"
 country = "Colombia"
 print("You're", name, "and you're from", country)
+print("You are age is: ", now - birthday)
