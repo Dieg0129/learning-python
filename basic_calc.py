38f7ee7a4c2aefd157a2045caa936ddfe153f8d5
DiegoA <diegoalex970006@gmail.com>
2024-01-20 09:20:56 -0500

    First commit basic_calc script: Features Calc v.1.0 and v.1.1

---
+++ basic_calc.py
@@ -0,0 +1,41 @@
+#El numeral se utiliza para escribir comentarios en el código en una línea y tres comillas simples para un bloque
+#Los comentarios permiten documentar
+
+#Script title: Basic calculator v1
+#Developer name: Joan C. Ayala
+#Date: 18-01-2024
+
+'''Script title: Basic calculator v1
+Developer name: Joan C. Ayala
+Date: 18-01-2024'''
+
+'''
+Dscripción: Esta calculadora permite obtener el resultado de las 4 operaciones matemáticas básicas de dos números (variables)
+'''
+
+#INPUTS
+num1 = 20
+num2 = 5
+
+#PROCESS
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#OUTPUTS
+print("BASIC CALC V1.0")
+print("Número 1: ", num1, type(num1))
+print("Número 1: ", num2, type(num2))
+print("El resultado de la suma es: ", add, type(add))
+print("El resultado de la resta es: ", sub, type(sub))
+print("El resultado de la multiplicación es: ", mul, type(mul))
+print("El resultado de la división es: ", div, type(div))
+print("\n")
+print("BASIC CALC V1.1")
+print("Número 1: ", num1)
+print("Número 1: ", num2)
+print("El resultado de la suma es: ", num1+num2)
+print("El resultado de la resta es: ", num1-num2)
+print("El resultado de la multiplicación es: ", num1*num2)
+print("El resultado de la división es: ", num1/num2)
