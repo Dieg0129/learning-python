ef109a352747012b3d357a47477663a2dca87085
DiegoA <diegoalex970006@gmail.com>
2024-01-22 23:37:44 -0500

    Proceso staging

---
+++ Proceso_staging.py
@@ -0,0 +1,10 @@
+'''Proceso staging
+
+Primer comando: Enviar paquete a staging o cola 
+git add . / git add --all / git add FILE_NAME.ext / git add FOLDER_NAME
+
+Segundo comando: anexar mensaje - ¿Qué contiene el paquete?
+git commit -m "Aqui se escribe un mensaje"
+
+Tercer comando: Enviar o desplegar el paquete hacía su destino (rama)
+git push origin NOMBRE_RAMA/BRANCH NAME / git push'''
